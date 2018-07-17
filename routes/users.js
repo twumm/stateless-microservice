b2ccc840b69905ec44642282d7375f6dedf67b61
twumm <martint.mensah@gmail.com>
2018-07-17 22:30:19 +0000

    feat: Define post route for user login.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const user_controller = require('../controllers/userController')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// Login user and sign JWT
+router.post('/login', user_controller.user_login_post)
 
 module.exports = router;
