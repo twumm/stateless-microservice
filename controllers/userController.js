486e66750389b34c64b2335dde4fa06f7d1e95d0
twumm <martint.mensah@gmail.com>
2018-07-17 23:01:19 +0000

    feat: Process user login request and create token with username.

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,9 +6,21 @@ exports.user_login_post = [
   // Validate input fields. Trim spaces around username
   body('username', 'Username required.').isLength({min: 3}).trim(),
   body('password', 'Password must atleast 6 characters.').isLength({ min:6 }),
-
   // Sanitize body with the wildcard.
   sanitizeBody('*'),
+
+  // Process the request after validating.
+  (req, res, next) => {
+    // Save errors from validation, if any.
+    const errors = validationResult(req);
+    // Save username and password. 
+    // Convert username to lowercase for db consistency
+    const username = req.body.username.toLowerCase()
+    const password = req.body.password
+    // Create a token for the user.
+    const token = jwt.sign({username: username}, process.env.jwtSecret, 
+                  {expiresIn: 21600 })
+  }
 ]
 
 
