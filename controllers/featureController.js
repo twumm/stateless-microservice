019004246356c125ad565b78b6c94cfad31babbc
twumm <martint.mensah@gmail.com>
2018-07-18 10:47:32 +0000

    feat: Resize image to 50x50 and save to desired location.

--- controllers/featureController.js
+++ controllers/featureController.js
@@ -21,8 +21,14 @@ exports.create_thumbnail_post = (req, res, next) => {
     // Download image from the url and save in selected destination in options.
     download.image(options)
       .then(({filename, image}) => {
-        
-        
+        // Resize image to 50x50 and save to desired location.
+        // Return conversion status to user.
+        sharp(filename)
+          .resize(50, 50)
+          .toFile(`${resizeFolder}output.${imageUrlExt}`, err => {
+            if (err) {return next(err)}
+            res.send(`File resized and saved to ${resizeFolder}`)
+        })
       })
       .catch(err => {
         console.error(err)
