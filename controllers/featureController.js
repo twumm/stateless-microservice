3f79e7649891a028cc301fa6d0eabcaf4aca319b
twumm <martint.mensah@gmail.com>
2018-07-19 12:05:02 +0000

    feat: Apply jsonpatch to object and return patched object.

--- controllers/featureController.js
+++ controllers/featureController.js
@@ -1,16 +1,20 @@
+const { body, validationResult } = require('express-validator/check')
+const { sanitizeBody } = require('express-validator/filter')
 const {fileExtension} = require('../middleware/customMiddleware')
 const imageTypes = ['jpg', 'tif', 'gif', 'png', 'svg']
 const sharp = require('sharp')
 const download = require('image-downloader')
+const jsonpatch = require('fast-json-patch')
 
 // Resize image on post
 exports.create_thumbnail_post = (req, res, next) => {
   // Save image url and extension
-  const imageUrl = req.body.image_url
-  const imageUrlExt = fileExtension(req.body.image_url)
+  const imageUrl = req.body.imageUrl
+  const imageUrlExt = fileExtension(imageUrl)
   
   // If image url extension is a type of image file, proceed to resize
   if (imageTypes.includes(imageUrlExt)) {
+    
     // Download image and save
     const options = {
       url: imageUrl,
@@ -18,6 +22,7 @@ exports.create_thumbnail_post = (req, res, next) => {
     }
     // Set location for resized images to be saved.
     const resizeFolder = './public/images/resized/'
+
     // Download image from the url and save in selected destination in options.
     download.image(options)
       .then(({filename, image}) => {
@@ -27,14 +32,41 @@ exports.create_thumbnail_post = (req, res, next) => {
           .resize(50, 50)
           .toFile(`${resizeFolder}output.${imageUrlExt}`, err => {
             if (err) {return next(err)}
-            res.send(`File resized and saved to ${resizeFolder}`)
+            res.json({converted: true, success: 'Image has been resized', thumbnail: resizeFolder})
         })
       })
       .catch(err => {
-        console.error(err)
+        res.status(400).json({error: 'Oops something went wrong. Kindly try again'})
       })
   }
   else {
-    res.send({error: 'We only handle image files.'})
+    res.json({error: 'We only handle image files.'})
   }
 }
+
+// Apply json patch to json object and return patched object.
+exports.patch_json_patch = [
+  // Validate input fields. Trim spaces around username
+  body('jsonObject', 'JSON object must not be empty.').isLength({min: 1}),
+  body('jsonPatchObject', 'JSON patch object must not be empty.').isLength({min: 1}),
+
+  // Process the request after validating.
+  (req, res, next) => {
+    // Save errors from validating, if any.
+    const errors = validationResult(req);
+
+    // Check if there were errors from the form.
+    if (!errors.isEmpty()) {
+      return res.status(400).json({errors: errors.array()})
+    }
+    else {
+      // Save object-to-patch and patch-object from the request.
+      let jsonObject = JSON.parse(req.body.jsonObject)
+      let jsonPatchObject = JSON.parse(req.body.jsonPatchObject)
+     
+      // Save patch in new variable.
+      let patchedObject = jsonpatch.applyPatch(jsonObject, jsonPatchObject).newDocument
+      res.json({patchedObject: patchedObject})
+    }
+  }
+]
