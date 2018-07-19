b091392ab2ef195b1b9299e362f3b5d9488f55d5
twumm <martint.mensah@gmail.com>
2018-07-19 13:16:19 +0000

    feat: Add verify token to routes to protect the routes.

--- routes/features.js
+++ routes/features.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const feature_controller = require('../controllers/featureController')
+const { verifyToken } = require('../middleware/customMiddleware')
 
+// Route to create image thumbnail.
+router.post('/create-thumbnail', verifyToken, feature_controller.create_thumbnail_post)
 
-// Test get
-router.post('/create-thumbnail', feature_controller.create_thumbnail_post)
+// Route to patch json objects.
+router.patch('/patch-object', verifyToken, feature_controller.patch_json_patch)
 
 module.exports = router;
