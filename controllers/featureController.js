fed7845a07ddba1aa3119aa691b417c60fa63ad8
twumm <martint.mensah@gmail.com>
2018-07-20 08:21:31 +0000

    refactor: Convert image extension to lower case, return image types required - on failure.

--- controllers/featureController.js
+++ controllers/featureController.js
@@ -6,15 +6,16 @@ const { fileExtension } = require('../middleware/customMiddleware')
 
 const imageTypes = ['jpg', 'tif', 'gif', 'png', 'svg']
 
-// Resize image on post
+// Resize image on post.
 exports.create_thumbnail_post = (req, res, next) => {
-  // Save image url and extension
+  // Save image url and extension.
   const { imageUrl } = req.body
-  const imageUrlExt = fileExtension(imageUrl)
+  // Save image extension. Convert to lowercase if in caps.
+  const imageUrlExt = fileExtension(imageUrl).toLowerCase()
 
-  // If image url extension is a type of image file, proceed to resize
+  // If image url extension is a type of image file, proceed to resize.
   if (imageTypes.includes(imageUrlExt)) {
-    // Download image and save
+    // Download image and save.
     const options = {
       url: imageUrl,
       dest: './public/images/original/',
@@ -24,23 +25,23 @@ exports.create_thumbnail_post = (req, res, next) => {
 
     // Download image from the url and save in selected destination in options.
     download.image(options)
-      .then(({ filename, image }) => {
+      .then(({ filename }) => {
         // Resize image to 50x50 and save to desired location.
         // Return conversion status to user.
         sharp(filename)
           .resize(50, 50)
           .toFile(`${resizeFolder}output.${imageUrlExt}`, (err) => {
             if (err) { return next(err) }
-            res.json({
+            return res.json({
               converted: true, user: req.user.username, success: 'Image has been resized', thumbnail: resizeFolder,
             })
           })
       })
       .catch(() => {
-        res.status(400).json({ error: 'Oops something went wrong. Kindly try again' })
+        res.status(400).json({ error: 'Oops something went wrong. Kindly check your image url and try again' })
       })
   } else {
-    res.json({ error: 'We only handle image files.' })
+    res.json({ error: `We only handle image files with extensions - ${[...imageTypes]}` })
   }
 }
 
