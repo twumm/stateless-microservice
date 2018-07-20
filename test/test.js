4e22aa989bde3e497b03e7a0a8829789d93fc42e
twumm <martint.mensah@gmail.com>
2018-07-20 12:51:04 +0000

    test: Add test for cases where invalid url is submitted, invalid token.

--- test/test.js
+++ test/test.js
@@ -11,6 +11,7 @@ describe('HackerBay Stateless Microservice', () => {
   let token
   // Set various variables to be used in the application
   const imageUrl = 'https://s3.amazonaws.com/oxfamamericaunwrapped.com/wp-content/uploads/2013/07/OAU10-53_pair_of_goats_2014-updated-image-400x400.jpg'
+  const invalidImageUrl = 'https://s3.amazonaws.com/oxfamamericaunwrapped.com/wp-content/uploads/2013/07/OAU10-53_pair_of_goats'
   const jsonObject = '{ "user": { "firstName": "Albert", "lastName": "Einstein" } }'
   const jsonPatchObject = '[{"op": "replace", "path": "/user/firstName", "value": "Leonado"}, {"op": "replace", "path": "/user/lastName", "value": "Da Vinci"}]'
 
@@ -51,6 +52,29 @@ describe('HackerBay Stateless Microservice', () => {
         })
       done()
     })
+
+    it('it should not process image if token is invalid', (done) => {
+      request.agent(app)
+        .post('/api/create-thumbnail')
+        .set('token', 'randomewwrongtoken')
+        .send({ imageUrl: imageUrl })
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(500)
+          expect(res.body.authorized).to.equal(false)
+        })
+      done()
+    })
+
+    it('it should not process image if url is invalid', (done) => {
+      request.agent(app)
+        .post('/api/create-thumbnail')
+        .set('token', token)
+        .send({ imageUrl: invalidImageUrl })
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(400)
+        })
+      done()
+    })
   })
 
   describe('Patch object', () => {
@@ -63,6 +87,18 @@ describe('HackerBay Stateless Microservice', () => {
           expect(res.statusCode).to.equal(200)
           done()
         })
+    })
+
+    it('it should not patch if token is invalid', (done) => {
+      request.agent(app)
+        .patch('/api/patch-object')
+        .set('token', 'randomewwrongtoken')
+        .send({ jsonObject, jsonPatchObject })
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(500)
+          expect(res.body.authorized).to.equal(false)
+        })
+      done()
     })
   })
 })
